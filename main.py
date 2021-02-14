3939e5306cb2163d95f0e4f44523c5db85f4d63a
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-02-14 10:17:44 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""In this project  we will learn about file system in python
+learn about read,write and open a file
+"""
+
+file = open("my_name.txt")  # this method open a file
+content_inside_of_file = file.read()  # this method read  a contents of a file and return a String .
+
+print(content_inside_of_file)
+
+
